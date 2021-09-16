71cd1831f8b53871de70b2fd9c2c295f0143c534
Andres Viloria <vilmoja58@gmail.com>
2021-09-16 11:55:56 -0500

    add modulos al formulario

---
+++ Controller/Constans.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined('URL_VIEWS')) define('URL_VIEWS', 'http://sql111.epizy.com/epiz_29731168_adsmimed/Views/');
+if (!defined('URL_CONTROLLER')) define('URL_CONTROLLER', 'http://sql111.epizy.com/epiz_29731168_adsmimed/Controller/');
+if (!defined('ADDRESS')) define('ADDRESS', 'C:/xampp/htdocs/epiz_29731168_adsmimed/Views/fotoproducto/');
+?>
+
